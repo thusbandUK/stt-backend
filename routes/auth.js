4dc795d3ccb5b820db48a13048d79ce21d5af9dc
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2023-06-06 10:48:45 +0100

    res DOT end added to POST logout

--- routes/auth.js
+++ routes/auth.js
@@ -71,7 +71,7 @@ router.post('/login', function(req, res, next) {
 router.post('/logout', function(req, res, next) {
   req.logout(function(err) {
     if (err) { return next(err); }
-    res.redirect('/');
+    res.end();
   });
 });
 
